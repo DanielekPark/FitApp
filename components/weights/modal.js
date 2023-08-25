41ec76395914733a647dfa559d8f698877729ae7
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2023-08-24 19:13:25 -0700

    added changes to modal component

--- components/weights/modal.js
+++ components/weights/modal.js
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
-import { Button, Dialog, CheckBox, ListItem, Avatar } from "@rneui/themed";
+import React from "react";
+import { Overlay, Button, Icon } from "@rneui/themed";
 import { View, Text, StyleSheet } from "react-native";
 import Calculator from "../calculator/calculator";
+import styles from "../../styles/style.module";
 
 const Modal = ({ setShowDialog, showDialog }) => {
   //closes modal window
-  const closeModal = () => setShowDialog(!showDialog);
+  const closeModal = () => setShowDialog(false);
 
   return (
     <View>
-      <Dialog isVisible={showDialog} onBackdropPress={closeModal}>
-        <Dialog.Title title=" " />
+      <Overlay isVisible={showDialog} onBackdropPress={closeModal}>
+      <Icon name="close" type="font-awesome" style={{marginLeft: 'auto'}} color='#33cccc' onPress={closeModal}/>
         <Calculator />
-      </Dialog>
+      </Overlay>
     </View>
   );
 };
